53b85498082827b2805ea9fee05881d881a30a87
guitarbien <guitarbien@gmail.com>
2017-11-01 02:08:37 +0800

    實作讀檔並產生 Candidate
    使用 Laravel Storage File

--- app/Services/Candidate.php
+++ app/Services/Candidate.php
@@ -4,5 +4,71 @@ namespace App\Services;
 
 class Candidate
 {
+    /** @var Config 所根據的 CONFIG 物件，由 constructor 傳入 */
+    private $config;
 
+    /** @var string 檔案的日期與時間 */
+    private $fileDateTime;
+
+    /** @var string 檔案名稱 */
+    private $name;
+
+    /** @var string 處理檔案的 process */
+    private $processName;
+
+    /** @var int 檔案大小 */
+    private $size;
+
+    /**
+     * Candidate constructor.
+     * @param array $info
+     */
+    public function __construct(array $info)
+    {
+        $this->config       = $info['config'];
+        $this->fileDateTime = $info['fileDateTime'];
+        $this->name         = $info['name'];
+        $this->processName  = $info['processName'];
+        $this->size         = $info['size'];
+    }
+
+    /**
+     * @return Config
+     */
+    public function getConfig(): Config
+    {
+        return $this->config;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileDateTime(): string
+    {
+        return $this->fileDateTime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProcessName(): string
+    {
+        return $this->processName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSize(): int
+    {
+        return $this->size;
+    }
 }
--- app/Services/MyBackupService.php
+++ app/Services/MyBackupService.php
@@ -4,6 +4,12 @@ namespace App\Services;
 
 use App\Services\Handlers\Handler;
 use App\Services\Handlers\HandlerFactory;
+use const DIRECTORY_SEPARATOR;
+use function explode;
+use function get_class;
+use Storage;
+use File;
+use function strtolower;
 
 /**
  * Class MyBackupService
@@ -43,12 +49,69 @@ class MyBackupService
         }
     }
 
+    /**
+     * 先隨便寫
+     * @return array
+     */
     private function findFiles(): array
     {
-        $fakeFileList = [""];
+        $fileList = ["xxxx.cs"];
 
-        $candidates[] = new Candidate();
+        /** @var Candidate[] $candidates */
+        $candidates = [];
+
+        foreach ($fileList as $filename) {
+            $info['config']       = $this->findMatchConfig($filename);
+            $info['fileDateTime'] = Storage::lastModified($filename);
+            $info['name']         = $this->getFileName($filename);
+            $info['processName']  = '???';
+            $info['size']         = Storage::size($filename);
+
+            $candidates[] = new Candidate($info);
+        }
+
         return $candidates;
+    }
+
+    /**
+     * 找出此檔案應使用何種 Config
+     * @param string $filename
+     * @return Config
+     */
+    private function findMatchConfig(string $filename): Config
+    {
+        $configManager = $this->getConfigManager();
+
+        /** @var Config $config */
+        foreach ($configManager as $config) {
+            if (strtolower($config['ext']) == strtolower(File::extension($filename))) {
+                return $config;
+            }
+        }
+    }
+
+    /**
+     * 由 list managers 中找出 ConfigManager
+     * @return ConfigManager
+     */
+    private function getConfigManager(): ConfigManager
+    {
+        foreach ($this->managers as $manager) {
+            if (get_class($manager) === 'ConfigManager') {
+                /** @var ConfigManager $manager */
+                return $manager;
+            }
+        }
+    }
+
+    /**
+     * 從 path 取得檔名
+     * @param string $filename
+     * @return string
+     */
+    private function getFileName(string $filename): string
+    {
+        return collect(explode(DIRECTORY_SEPARATOR, $filename))->last();
     }
 
     private function broadcastToHandlers(Candidate $candidate): void
@@ -68,13 +131,11 @@ class MyBackupService
         $handlers[] = HandlerFactory::create('file');
 
         // 讀取 config 的 handlers
-        $configHandlers = [];
-        foreach ($configHandlers as $handler) {
+        foreach ($candidate['handlers'] as $handler) {
             $handlers[] = $handler;
         }
 
-        $configDestination = 'directory';
-        $handlers[] = HandlerFactory::create($configDestination);
+        $handlers[] = HandlerFactory::create($candidate['destination']);
     }
 
 
