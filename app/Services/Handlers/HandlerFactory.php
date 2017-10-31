b94f82277e73d20e49db53c54d755ca02ae441c5
guitarbien <guitarbien@gmail.com>
2017-11-01 02:09:16 +0800

    FileFactory 讀取 handler_mapping.json 產生 Handler

--- app/Services/Handlers/HandlerFactory.php
+++ app/Services/Handlers/HandlerFactory.php
@@ -2,9 +2,20 @@
 
 namespace App\Services\Handlers;
 
+use function json_decode;
+use Storage;
+
 class HandlerFactory
 {
-    public static function create($string): Handler
+    /** @const string handler對應檔 */
+    const HANDLER_MAPPING_FILE = 'handler_mapping.json';
+
+    public static function create(string $handleType): ?Handler
     {
+        $handlerMapping = json_decode(Storage::get(self::HANDLER_MAPPING_FILE), true);
+
+        if (isset($handlerMapping[$handleType])) {
+            return new $handlerMapping[$handleType]();
+        }
     }
 }
