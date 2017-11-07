48c2051bcc8fe750f85fc980cbe2e3e9f1429670
guitarbien <guitarbien@gmail.com>
2017-11-08 00:39:31 +0800

    直接對 FileFinder 做 foreach，當迴圈跑到每個檔案時，才產生 Candidate 物件，並對各 handler 做廣播

--- app/Services/MyBackupService.php
+++ app/Services/MyBackupService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Services\Handlers\FileFinderFactory;
 use App\Services\Handlers\Handler;
 use App\Services\Handlers\HandlerFactory;
 use Illuminate\Support\Facades\File;
@@ -15,12 +16,6 @@ class MyBackupService
 {
     /** @var JsonManager[] 存放各種 Manager 的陣列 */
     private $managers = [];
-
-    /** @const string config unit 處理單位 檔案 */
-    const UNIT_STRING_FILE = 'file';
-
-    /** @const string config unit 處理單位 目錄 */
-    const UNIT_STRING_DIRECTORY = 'directory';
 
     /**
      * MyBackupService constructor.
@@ -46,51 +41,16 @@ class MyBackupService
      */
     public function doBackup(): void
     {
-        $candidates = $this->findFiles();
-
-        foreach ($candidates as $candidate) {
-            $this->broadcastToHandlers($candidate);
-        }
-    }
-
-    /**
-     * 先隨便寫
-     * @return Candidate[]
-     */
-    private function findFiles(): array
-    {
-        /** @var Candidate[] $candidates */
-        $candidates = [];
-
-        // 讀 config.json 決定要去哪個路徑抓資料
         foreach ($this->getConfigManager() as $config) {
-            /** @var Config $config */
-            // 應對目錄或是檔案做處理
-            if ($config->getUnit() === self::UNIT_STRING_FILE) {
-                $fileList = File::get($config->getLocation());
-            } else {
-                $fileList = File::files($config->getLocation());
-            }
+            // 目前先實作本機
+            // (之後也許根據 location 來切換 local s3 ftp ?)
+            $fileFinder = FileFinderFactory::create('file', $config);
 
-            // 讀取每個設定檔的 location 下的檔案清單
-            foreach ($fileList as $file) {
-                /** @var SplFileInfo $file */
-                // 只處理符合 extension 的 file
-                if ($config->getExt() !== $file->getExtension()) {
-                    continue;
-                }
-
-                $info['config']       = $config;
-                $info['fileDateTime'] = $file->getMTime();
-                $info['name']         = $file->getPathname();
-                $info['processName']  = '???';
-                $info['size']         = $file->getSize();
-
-                $candidates[] = new Candidate($info);
+            // iterate 到 current 時回傳 candidate
+            foreach ($fileFinder as $candidate) {
+                $this->broadcastToHandlers($candidate);
             }
         }
-
-        return $candidates;
     }
 
     /**
