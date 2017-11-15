6b1c3a3fed7eb5e75d781fcb3b7a6ff551e9325b
guitarbien <guitarbien@gmail.com>
2017-11-16 00:30:02 +0800

    EveryDay 也要處理

--- app/Services/ScheduledTask.php
+++ app/Services/ScheduledTask.php
@@ -18,14 +18,17 @@ class ScheduledTask extends AbstractTask
 
         while (true) {
             // 檢查頻率時間
-            if (date("l") === $schedule->getInterval() && date("H:i") === $schedule->getTime()) {
-
+            if ((date("l") === $schedule->getInterval() || "Everyday" === $schedule->getInterval()) &&
+                date("H:i") === $schedule->getTime())
+            {
                 foreach ($this->fileFinder as $candidate) {
                     $this->broadcastToHandlers($candidate);
                 }
 
                 break;
             }
+
+            // todo 可再加上最大時間區間防呆
         }
     }
 }
